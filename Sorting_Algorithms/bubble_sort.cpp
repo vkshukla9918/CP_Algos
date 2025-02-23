4ac6d323898f29cb3910eff32b02c9ab9186c1bb
Vkshukla9918 <vkshukla9918@gmail.com>
2025-02-23 13:52:56 +0530

    change in variable

--- Sorting_Algorithms/bubble_sort.cpp
+++ Sorting_Algorithms/bubble_sort.cpp
@@ -1,14 +1,14 @@
 # include<bits/stdc++.h>
 using namespace std;
 
-void bubbleSort(vector<int> &arr){
-    int n = arr.size();
+void bubbleSort(vector<int> &vec){
+    int n = vec.size();
     bool swapped;
     for(int i = 0;i<n-1;i++){
         swapped = false;
         for(int j =0;j<n-i-1; j++){
-            if(arr[j]> arr[j+1]){
-            swap(arr[j], arr[j+1]);
+            if(vec[j]> vec[j+1]){
+            swap(vec[j], vec[j+1]);
             swapped = true;
             }
         }
